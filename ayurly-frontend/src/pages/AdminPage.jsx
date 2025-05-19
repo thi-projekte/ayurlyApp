61410e57ee61bc6fe1e9101fdcb3c21546008914
Lorenz0045 <lorenz.schermbach@outlook.de>
2025-05-19 23:26:04 +0200

    importanpassung

--- ayurly-frontend/src/pages/AdminPage.jsx
+++ ayurly-frontend/src/pages/AdminPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styles from './AdminPage.module.css';
 
