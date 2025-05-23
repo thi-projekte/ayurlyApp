b8c307a41dfd276c5de2223dce5f95d3b779915c
Lorenz0045 <lorenz.schermbach@outlook.de>
2025-05-23 05:03:19 +0200

    style fix

--- ayurly-frontend/src/main.jsx
+++ ayurly-frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
-import './styles/indexStyles.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
