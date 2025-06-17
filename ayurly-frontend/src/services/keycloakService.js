2d6cace938c50bc004196e27b8dddb9fd9f88198
Lorenz0045 <lorenz.schermbach@outlook.de>
2025-06-17 23:12:27 +0200

    revert

--- ayurly-frontend/src/services/keycloakService.js
+++ ayurly-frontend/src/services/keycloakService.js
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 // KC_HOSTNAME aus docker-compose.yml ist hier wichtig für die URL
 const keycloakConfig = {
-  url: '/auth',                                    
+  url: 'https://keycloak.winfprojekt.de',                                    
   realm: 'Aryuveda', 
   clientId: 'ayurly-frontend', // Frontend-Client-ID in Keycloak
 };
